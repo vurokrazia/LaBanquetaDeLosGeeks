36a45a121c21c3ce848091eb2dc303a59969cb05
jesus martinez <jesus.alberto.vk@gmail.com>
2021-01-01 01:09:27 -0600

    Template Finish

---
+++ src/index.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const App = () => (
+  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat enim architecto doloremque voluptatem tempore vel similique, molestias, odio ipsam consequatur eius odit accusamus aliquid dolorem corporis consectetur mollitia inventore sapiente?</p>
+);
+
+ReactDOM.render(
+  <App />,
+  document.getElementById("root")
+);
---
+++ webpack.config.ts
@@ -0,0 +1,52 @@
+import path from "path";
+import webpack from "webpack";
+import ForkTsCheckerWebpackPlugin from 'fork-ts-checker-webpack-plugin';
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const config: webpack.Configuration = {
+  entry: "./src/index.tsx",
+  module: {
+    rules: [
+      {
+        test: /\.(ts|js)x?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: [
+              "@babel/preset-env",
+              "@babel/preset-react",
+              "@babel/preset-typescript",
+            ],
+          },
+        },
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".tsx", ".ts", ".js"],
+  },
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js",
+  },
+  devServer: {
+    contentBase: path.join(__dirname, "dist"),
+    compress: true,
+    port: 4000,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './public/index.html',
+      filename: './index.html'
+    }),
+    new ForkTsCheckerWebpackPlugin({
+      async: false,
+      eslint: {
+        files: "./src/**/*",
+      },
+    }),
+  ],
+};
+
+export default config;
