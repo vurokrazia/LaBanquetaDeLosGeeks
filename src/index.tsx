84f06008879e26f8274bb8a7c44bd3065e421e05
jesus martinez <jesus.alberto.vk@gmail.com>
2021-01-02 14:02:50 -0600

    Remove TS

--- src/index.tsx
+++
@@ -1,6 +0,0 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import './App.css'
-const App = () => <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat </p>
-
-ReactDOM.render(<App />, document.getElementById('root'))
